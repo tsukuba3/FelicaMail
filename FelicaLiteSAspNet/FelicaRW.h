8292dc4fca5ac2ccadfa850a481e08511b133ea6
tsukuba3 <terushige.fujimaki@me.com>
2016-11-24 05:50:09 +0900

    初回のコミット。

---
+++ FelicaLiteSAspNet/FelicaRW.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "felica_nfc_library.h"
+
+#pragma comment(lib ,"felica_nfc_library.lib")
+//#pragma warning(disable : 4996)
+
+
+
+// FelicaRW
+#define IDM_SIZE        (8)         // IDm のサイズ
+#define BLOCK_SIZE      (16)        // ブロック（読み書きの最小単位）のサイズ
+
+#define SYSTEM_CODE     (0x88b4)    // 0x88b4 = FeliCa Lite-S のシステムコード
+#define SERVICE_CODE_RW (0x0009)    // 0x0009 = Random Read/Write のサービスコード
+#define SERVICE_CODE_RO (0x000B)    // 0x000B = Random Read Only のサービスコード
+
+public bool PrintNfcLastErr(char* pFuncName);
+public bool PrintHex(unsigned char *pBuf, int iLen);
+public bool MyPolling(unsigned short usSystemCode, unsigned char pIDm[IDM_SIZE]);
+public bool MyWriteWithoutEncryption(unsigned char *pIDm, unsigned short usServiceCode, unsigned char ucBlockOffset, unsigned char *pDataW);
+public bool MyReadWithoutEncryption(unsigned char *pIDm, unsigned short usServiceCode, unsigned char ucBlockOffset, unsigned char *pDataR);
+public int FelicaRW::FelicaRW_main(int block, unsigned char* wData, bool read);
---
+++ FelicaLiteSAspNet/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FelicaLiteSAspNet
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("MyFelicaLiteS.dll")]
+        public extern static IntPtr FelicaRW_main(int block, Byte[] wData, bool read);
+        [DllImport("MyFelicaLiteS.dll")]
+        public extern static IntPtr GetName();
+        [DllImport("MyFelicaLiteS.dll")]
+        public extern static IntPtr GetData();
+
+        int bankNum = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for(int i=0;i<16;i++)
+                Bank.Items.Add(i);
+
+            Bank.SelectedIndex = 0;
+        }
+
+        unsafe private void Write_Click(object sender, RoutedEventArgs e)
+        {
+            bankNum = Bank.SelectedIndex;
+
+            Byte[] wData = Encoding.ASCII.GetBytes(Line.Text);
+
+            FelicaRW_main(bankNum, wData, false);
+
+            MessageBox.Show("書き込みが完了しました");
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            FelicaRW_main(0, null, true);
+
+            IntPtr po = GetData();
+            String str = Marshal.PtrToStringAnsi(po);
+
+            String[] args = str.Split(',');
+
+            po = GetName();
+            String name = Marshal.PtrToStringUni(po);
+
+            LName.Content = name;
+            Userid.Text = args[2];
+            Password.Text = args[3];
+            Account.Text = args[0];
+            Domain.Text = args[1];
+
+        }
+
+        private void MailSend_Click(object sender, RoutedEventArgs e)
+        {
+            String url = @"http://konekophoto.azurewebsites.net/FelicaMail?user_id=" + Userid.Text + "&password=" + Password.Text + "&mail_address=" + Account.Text + "@" + Domain.Text;
+
+            var client = new WebClient();
+            client.OpenRead(url);
+
+        }
+    }
+}
